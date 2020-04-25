246c1ff39690aec27e40471e22ad68e59c9426cb
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-04-25 18:16:34 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -9,7 +9,17 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+def crew
+  binding.pry
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+end
+
+def greet_crew (crew)
+  crew.map {|crew_member| "Hello #{crew_member}."}
+end
+
 def engage
-  puts state_log(date)
   date = generate_star_date
+puts  state_log(date)
+  greet_crew(crew)
 end
