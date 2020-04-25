74c9497a282bf8db5da7879c2f2bff8ac40adb2e
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-04-25 18:13:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -18,6 +18,7 @@ def greet_crew (crew)
 end
 
 def engage
+  binding.pry
   date = generate_star_date
 puts  state_log(date)
   greet_crew(crew)
